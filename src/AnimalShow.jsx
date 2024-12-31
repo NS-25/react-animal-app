d28daa5da96b2cf9b3d045d6c5a6e1bb44ee227f
George-25 <georgemallory26@gmail.com>
2024-12-30 20:36:36 -0500

    updated image with heart and style it

--- src/AnimalShow.jsx
+++ src/AnimalShow.jsx
@@ -1,10 +1,11 @@
-import bird from './svg/bird.svg';
+import bird from "./svg/bird.svg";
 import cat from "./svg/cat.svg";
 import cow from "./svg/cow.svg";
 import dog from "./svg/dog.svg";
 import gator from "./svg/gator.svg";
 import heart from "./svg/heart.svg";
 import horse from "./svg/horse.svg";
+import { useState } from "react";
 
 const svgMap = {
   // Whenever we are writing out an abject, where a key is identical to the value sa the exact same word, we can abbreviate it as just one word.
@@ -14,13 +15,22 @@ const svgMap = {
   cow,
   dog,
   gator,
-  horse
-}
+  horse,
+};
 const AnimalShow = ({ type }) => {
+  const [clicks, setClicks] = useState(0);
+
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
   return (
     <>
-      <div>
-        <img alt="animal" src={svgMap[type]}/>
+      <div onClick={handleClick}>
+        <img alt="animal" src={svgMap[type]} />
+        <img 
+        src={heart}
+         alt="heart"
+         style={{width: 10 + 10 * clicks}} />
       </div>
     </>
   );
